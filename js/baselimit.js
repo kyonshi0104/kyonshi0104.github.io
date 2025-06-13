b2b020a3fad87f4e53ab45b7df00fa5422067406
Kyonshi <155651185+kyonshi0104@users.noreply.github.com>
2025-06-13 22:53:44 +0900

    Update baselimit.js

--- js/baselimit.js
+++ js/baselimit.js
@@ -3,3 +3,9 @@ const currentUrl = window.location.href;
 if (currentUrl.endsWith("base.html")) {
     window.location.href = "/";
 }
+
+window.onload = function() {
+    if (/Mobi|Android|iPhone|iPad|iPod/.test(navigator.userAgent)) {
+        alert("現在モバイルデバイスに対応してないです\nほんとすいません");
+    }
+};
