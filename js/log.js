d9edbdc4c9aa825c753371508f7d560275effab7
Kyonshi <155651185+kyonshi0104@users.noreply.github.com>
2025-06-14 12:10:04 +0900

    Update log.js

--- js/log.js
+++ js/log.js
@@ -53,6 +53,7 @@ if (window.self === window.top && !['127.0.0.1', 'localhost'].includes(window.lo
 document.addEventListener("load", function () {
     const isMobile = /Mobi|Android|iPhone|iPad|iPod/.test(navigator.userAgent);
     if (isMobile) {
+        console.log("モバイルデバイスだと思われ")
         alert("現在モバイルデバイスに対応していません\nほんとすいません");
     }
 });
